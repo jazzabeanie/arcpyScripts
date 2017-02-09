40552ef8502c0aba0f8bdd727ff9266752f5e54e
Jared Johnston <jazzab@gmail.com>
2017-02-09 14:10:37 +1000

    WIP: Start analysis by for loop

--- create_GMZ_catchments.py
+++ create_GMZ_catchments.py
@@ -1,4 +1,4 @@
-#--------------------------------
+# --------------------------------
 # Name: create_GMZ_catchments
 # Purpose: to create an area for each point that includes all GMZs whose
 # centroid falls within the a buffer distance.
@@ -8,24 +8,39 @@
 # ArcGIS Version:   10.2
 # Python Version:   2.7
 # Source: https://blogs.esri.com/esri/arcgis/2011/08/04/pythontemplate/
-#--------------------------------
-import os
-import sys
+# --------------------------------
+import os # noqa
+import sys # noqa
 import arcpy
 import logging
 
-logging.basicConfig(filename='arcpyTemplate.log', # TODO: update log filename
-                                            level=logging.DEBUG,
-                                            format='%(asctime)s %(message)s')
+logging.basicConfig(filename='arcpyTemplate.log',  # TODO: update log filename
+                    level=logging.DEBUG,
+                    format='%(asctime)s %(message)s')
 
 # Commonly used layers: # TODO: expand this section
 GMZ = r'R:\InfrastructureModels\Growth\Database\GrowthModelGMZ.mdb\GMZ'
-sde_properties = "O:\\Data\\Planning_IP\\Spatial\\WindowAuth@Mapsdb01@SDE_Vector.sde\\sde_vector.TCC.Cadastral\\sde_vector.TCC.Properties"
+sde_properties = ("O:\\Data\\Planning_IP\\Spatial\\"
+                  "WindowAuth@Mapsdb01@SDE_Vector.sde\\"
+                  "sde_vector.TCC.Cadastral""\\sde_vector.TCC.Properties")
+
 
 def do_analysis(*argv):
-    """TODO: Add documentation about this function here"""
+    """This function make a catchment for each point in a feature class. Takes 2 args:
+        1. file with points so make catchments for
+        2. file with polygons to use to make catchments"""
     try:
-        #TODO: Add analysis here
+        points = argv[0]
+        catchments = argv[1]
+        cursor = arcpy.SearchCursor(points)
+        for row in cursor:  # for each park point:
+            # print(row.getValue(field))
+        # TODO: Add analysis here
+            # select GMZs whose centroid is within a buffer distance
+            # export selection to a temporary layer
+            # merge all the features in the temporary layer
+            # copy the 1 feature in the temporary layer into the output layer
+        # save output layer
         pass
     except arcpy.ExecuteError:
         print arcpy.GetMessages(2)
@@ -40,7 +55,11 @@ def do_analysis(*argv):
 # as a geoprocessing script tool, or as a module imported in
 # another script
 if __name__ == '__main__':
-    # Arguments are optional
-    argv = tuple(arcpy.GetParameterAsText(i)
-        for i in range(arcpy.GetArgumentCount()))
-    do_analysis(*argv) # https://docs.python.org/2.7/tutorial/controlflow.html#unpacking-argument-lists
+    # Arguments overwrite defaults
+    default_points = (r'O:\Data\IC\Spatial Data\LGIP\Database'
+                      '\scratchworkspace.gdb\existing_districtRec_parks_points')
+    argv = [default_points, GMZ]
+    if arcpy.GetArgumentCount() != 0:
+        argv = tuple(arcpy.GetParameterAsText(i)
+                    for i in range(arcpy.GetArgumentCount()))
+    do_analysis(*argv) # see here for help on #argv https://docs.python.org/2.7/tutorial/controlflow.html#unpacking-argument-lists # noqa
