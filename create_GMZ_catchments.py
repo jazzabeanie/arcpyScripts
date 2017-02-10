685c79e797f35b2427d7dfc61680644fbed9d79a
Jared Johnston <jazzab@gmail.com>
2017-02-10 13:09:57 +1000

    Update create_GMZ_catchments
    The tool has been essentially finished. The only thing I could do is get
    a list of all the GMZs that have been joined to create the final
    catchment shape.

--- create_GMZ_catchments.py
+++ create_GMZ_catchments.py
@@ -14,7 +14,7 @@ import sys # noqa
 import arcpy
 import logging
 
-logging.basicConfig(filename='arcpyTemplate.log',  # TODO: update log filename
+logging.basicConfig(filename='create_GMZ_catchments.log',
                     level=logging.DEBUG,
                     format='%(asctime)s %(message)s')
 
@@ -25,22 +25,42 @@ sde_properties = ("O:\\Data\\Planning_IP\\Spatial\\"
                   "sde_vector.TCC.Cadastral""\\sde_vector.TCC.Properties")
 
 
+def delete_if_exists(layer):
+    """Deleted the passed in layer if it exists. This avoids errors."""
+    if arcpy.Exists(layer):
+        logging.warning("Deleting %s" % layer)
+        arcpy.Delete_management(layer)
+
+
 def do_analysis(*argv):
     """This function make a catchment for each point in a feature class.
     Takes 2 args:
         1. file with points so make catchments for
-        2. file with polygons to use to make catchments"""
+        2. file with polygons to use to make catchments
+    """
     try:
         points = argv[0]
         catchments = argv[1]
         output = argv[2]
-        arcpy.SpatialJoin_analysis (catchments, points, output, {join_operation}, {join_type}, {field_mapping}, {match_option}, {search_radius}, {distance_field_name}) #TODO: finish this spatial join
-        # TODO: Complete analysis
-        # Spatial join points to GMZs base on buffer
-            # join only LGIP_ID of the park
-        # merge GMZs based on common LGIP_ID
-        # save output layer
-        pass
+        output1 = (r'O:\Data\IC\Spatial Data\LGIP\Database'
+                   r'\scratchworkspace.gdb'
+                   r'\tmp_output1')
+        delete_if_exists(output)
+        delete_if_exists(output1)
+        GMZ_fieldmap = arcpy.FieldMap()
+        GMZ_fieldmap.addInputField(catchments, "GMZ")
+        ID_fieldmap = arcpy.FieldMap()
+        ID_fieldmap.addInputField(points, "ID")
+        field_mapping = arcpy.FieldMappings()
+        field_mapping.addFieldMap(GMZ_fieldmap)
+        field_mapping.addFieldMap(ID_fieldmap)
+        arcpy.SpatialJoin_analysis(catchments, points, output1,
+                                   "JOIN_ONE_TO_MANY", "KEEP_COMMON",
+                                   field_mapping, "HAVE_THEIR_CENTER_IN",
+                                   2000)
+        arcpy.Dissolve_management(output1, output,
+                                  "ID", "",
+                                  "", "")
     except arcpy.ExecuteError:
         print arcpy.GetMessages(2)
         logging.warning(arcpy.GetMessages(2))
@@ -48,6 +68,7 @@ def do_analysis(*argv):
         print e.args[0]
         logging.warning(e.args[0])
 # End do_analysis function
+
 
 # This test allows the script to be used from the operating
 # system command prompt (stand-alone), in a Python IDE,
