33f300c541d31db35abbcac79d4d14fd3600c1c4
Jared Johnston <jazzab@gmail.com>
2017-10-11 15:33:57 +1000

    Add field_in_feature_class method
    This make checking for the existence of fiels easier.

--- jj_methods.py
+++ jj_methods.py
@@ -19,6 +19,145 @@ arcpy.env.workspace = r'O:\Data\Planning_IP\Admin\Staff\Jared\GIS\Tools\arcpyScr
 logging = logging.getLogger(__name__)
 
 
+# TODO: create a bunch of functions that create test data sets. For example: add the function below, but let the list of verticies be passed in as an argument.
+# def create_test_area_polygon(output):
+#     """
+#     returns an array of points used to create a polygon that will be used to create the test case.
+#     """
+#     print "directory = " + jj.get_directory_from_path(output)
+#     print "name = " + jj.get_file_from_path(output)
+#     arcpy.CreateFeatureclass_management(
+#             jj.get_directory_from_path(output), # out_path
+#             jj.get_file_from_path(output), # out_name
+#             "POLYGON") # geometry_type
+#             # "#", # template
+#             # "DISABLED", # has_m
+#             # "DISABLED", # has_z
+#             # inputs["growth_model_polygon"]) # spatial_reference
+#     array = arcpy.Array([arcpy.Point(471966.2812500,7858694.5000000),
+#                          arcpy.Point(471895.5000000,7858701.0000000),
+#                          arcpy.Point(471865.5000000,7858703.5000000),
+#                          arcpy.Point(471844.0312500,7858706.0000000),
+#                          arcpy.Point(471622.6875000,7858726.0000000),
+#                          arcpy.Point(471389.3750000,7858747.5000000),
+#                          arcpy.Point(469967.9687500,7858874.5000000),
+#                          arcpy.Point(469973.7812500,7858974.5000000),
+#                          arcpy.Point(469972.3125000,7858999.0000000),
+#                          arcpy.Point(469971.6875000,7859029.0000000),
+#                          arcpy.Point(469975.3125000,7859051.0000000),
+#                          arcpy.Point(469980.9062500,7859076.0000000),
+#                          arcpy.Point(469976.3125000,7859121.5000000),
+#                          arcpy.Point(469971.6875000,7859153.0000000),
+#                          arcpy.Point(469970.0625000,7859174.0000000),
+#                          arcpy.Point(469974.3437500,7859201.5000000),
+#                          arcpy.Point(469998.0312500,7859241.0000000),
+#                          arcpy.Point(470036.7812500,7859313.0000000),
+#                          arcpy.Point(470047.4062500,7859344.0000000),
+#                          arcpy.Point(470069.3750000,7859444.0000000),
+#                          arcpy.Point(470073.0937500,7859478.0000000),
+#                          arcpy.Point(470075.0625000,7859514.5000000),
+#                          arcpy.Point(470074.8125000,7859556.0000000),
+#                          arcpy.Point(470073.5625000,7859620.0000000),
+#                          arcpy.Point(470067.9062500,7859716.0000000),
+#                          arcpy.Point(470046.4062500,7859801.5000000),
+#                          arcpy.Point(470010.3750000,7859843.5000000),
+#                          arcpy.Point(469898.5312500,7859872.5000000),
+#                          arcpy.Point(469845.2187500,7859882.0000000),
+#                          arcpy.Point(469831.9062500,7859899.0000000),
+#                          arcpy.Point(469831.1562500,7859915.0000000),
+#                          arcpy.Point(469830.7500000,7859925.0000000),
+#                          arcpy.Point(469847.1875000,7859991.0000000),
+#                          arcpy.Point(469849.1875000,7860075.0000000),
+#                          arcpy.Point(469851.6562500,7860109.0000000),
+#                          arcpy.Point(469847.4375000,7860172.0000000),
+#                          arcpy.Point(469838.5625000,7860231.0000000),
+#                          arcpy.Point(469840.0312500,7860251.5000000),
+#                          arcpy.Point(469818.8125000,7860315.0000000),
+#                          arcpy.Point(469801.7812500,7860347.0000000),
+#                          arcpy.Point(469815.8437500,7860404.0000000),
+#                          arcpy.Point(469832.2187500,7860506.5000000),
+#                          arcpy.Point(469837.5000000,7860506.0000000),
+#                          arcpy.Point(470369.8750000,7860453.5000000),
+#                          arcpy.Point(470400.2812500,7860773.0000000),
+#                          arcpy.Point(470480.0937500,7860765.5000000),
+#                          arcpy.Point(470515.1250000,7860762.0000000),
+#                          arcpy.Point(470538.5312500,7860760.0000000),
+#                          arcpy.Point(470558.4375000,7860758.0000000),
+#                          arcpy.Point(470578.3437500,7860756.0000000),
+#                          arcpy.Point(470598.2500000,7860754.5000000),
+#                          arcpy.Point(470618.1562500,7860752.5000000),
+#                          arcpy.Point(470638.0625000,7860750.5000000),
+#                          arcpy.Point(470657.9687500,7860748.5000000),
+#                          arcpy.Point(470677.8750000,7860747.0000000),
+#                          arcpy.Point(470697.7812500,7860745.0000000),
+#                          arcpy.Point(470717.6875000,7860743.0000000),
+#                          arcpy.Point(470737.5937500,7860741.0000000),
+#                          arcpy.Point(470757.5000000,7860739.5000000),
+#                          arcpy.Point(470777.4062500,7860737.5000000),
+#                          arcpy.Point(470797.3125000,7860735.5000000),
+#                          arcpy.Point(470795.4375000,7860715.5000000),
+#                          arcpy.Point(470794.0312500,7860700.5000000),
+#                          arcpy.Point(470793.3125000,7860693.0000000),
+#                          arcpy.Point(470792.8750000,7860688.5000000),
+#                          arcpy.Point(470792.0937500,7860680.0000000),
+#                          arcpy.Point(470790.9375000,7860668.0000000),
+#                          arcpy.Point(470789.0625000,7860648.0000000),
+#                          arcpy.Point(470828.8750000,7860644.0000000),
+#                          arcpy.Point(470848.7812500,7860642.5000000),
+#                          arcpy.Point(470868.6875000,7860640.5000000),
+#                          arcpy.Point(470888.5937500,7860638.5000000),
+#                          arcpy.Point(470908.4687500,7860636.5000000),
+#                          arcpy.Point(470928.3750000,7860635.0000000),
+#                          arcpy.Point(470948.2812500,7860633.0000000),
+#                          arcpy.Point(470973.3125000,7860630.5000000),
+#                          arcpy.Point(470975.9375000,7860658.5000000),
+#                          arcpy.Point(470976.1875000,7860661.0000000),
+#                          arcpy.Point(470987.2812500,7860672.5000000),
+#                          arcpy.Point(470995.2187500,7860757.5000000),
+#                          arcpy.Point(471005.2500000,7860756.5000000),
+#                          arcpy.Point(471035.1875000,7860753.5000000),
+#                          arcpy.Point(471067.5312500,7860750.5000000),
+#                          arcpy.Point(471077.6562500,7860749.5000000),
+#                          arcpy.Point(471086.9687500,7860845.0000000),
+#                          arcpy.Point(471162.7812500,7860838.0000000),
+#                          arcpy.Point(471251.1250000,7860842.0000000),
+#                          arcpy.Point(471258.9687500,7860704.0000000),
+#                          arcpy.Point(471344.4062500,7860709.0000000),
+#                          arcpy.Point(471351.2187500,7860594.5000000),
+#                          arcpy.Point(471357.3437500,7860560.5000000),
+#                          arcpy.Point(471362.5000000,7860542.0000000),
+#                          arcpy.Point(471371.6875000,7860518.0000000),
+#                          arcpy.Point(471385.0625000,7860487.0000000),
+#                          arcpy.Point(471400.4062500,7860457.5000000),
+#                          arcpy.Point(471411.0937500,7860441.5000000),
+#                          arcpy.Point(471420.4375000,7860429.5000000),
+#                          arcpy.Point(471431.9375000,7860417.0000000),
+#                          arcpy.Point(471448.7500000,7860403.5000000),
+#                          arcpy.Point(471465.8437500,7860392.5000000),
+#                          arcpy.Point(471492.5625000,7860381.0000000),
+#                          arcpy.Point(471534.2187500,7860371.5000000),
+#                          arcpy.Point(471561.5000000,7860370.5000000),
+#                          arcpy.Point(471590.6250000,7860372.0000000),
+#                          arcpy.Point(471619.5625000,7860379.5000000),
+#                          arcpy.Point(471642.4375000,7860389.5000000),
+#                          arcpy.Point(471872.5312500,7860509.5000000),
+#                          arcpy.Point(471989.6875000,7860139.0000000),
+#                          arcpy.Point(472010.8125000,7860053.0000000),
+#                          arcpy.Point(472027.3125000,7859953.5000000),
+#                          arcpy.Point(472037.2812500,7859795.5000000),
+#                          arcpy.Point(472004.7500000,7859272.5000000),
+#                          arcpy.Point(471964.3437500,7858942.5000000),
+#                          arcpy.Point(471959.8750000,7858871.5000000),
+#                          arcpy.Point(471959.3125000,7858801.0000000),
+#                          arcpy.Point(471962.7812500,7858728.0000000),
+#                          arcpy.Point(471966.2812500,7858694.5000000)])
+#     polygon = arcpy.Polygon(array)
+#     # Open an InsertCursor and insert the new geometry
+#     cursor = arcpy.da.InsertCursor(output, ['SHAPE@'])
+#     cursor.insertRow([polygon])
+#     del cursor
+
+
 def delete_if_exists(layer):
     """Deleted the passed in layer if it exists. This avoids errors."""
     if arcpy.Exists(layer):
@@ -29,6 +168,19 @@ def delete_if_exists(layer):
 def arguments_exist():
     """Returns true if the file was called with arguments, false otherwise."""
     if arcpy.GetArgumentCount() != 0:
+        return True
+    else:
+        return False
+
+
+def field_in_feature_class(field_name, feature_class):
+    """
+    returns true if field (parameter 1), exists in feature class (parameter 2). returns false if not.
+    See: https://epjmorris.wordpress.com/2015/04/22/how-can-i-check-to-see-if-an-attribute-field-exists-using-arcpy/
+    """
+    # return field_name in [field.name for field in arcpy.ListFields(feature_class)]
+    fields = arcpy.ListFields(feature_class, field_name)
+    if len(fields) == 1:
         return True
     else:
         return False
@@ -112,6 +264,7 @@ def test_print():
 # as a geoprocessing script tool, or as a module imported in
 # another script
 if __name__ == '__main__':
+    # TODO: set up logging so that I don't see 'No handlers could be found for logger "__main__"'
     print "Running test"
     print ""
 
@@ -133,6 +286,29 @@ if __name__ == '__main__':
         print "  This file was called with arguments"
     else:
         print "  This file was not called with arguments"
+    print "------"
+
+    print "Testing field_in_feature_class finds an existing field..."
+    test_feature_class = "testing_field_in_feature_class"
+    delete_if_exists(test_feature_class)
+    arcpy.CreateFeatureclass_management(
+            arcpy.env.workspace, # out_path
+            test_feature_class, # out_name
+            "POLYGON") # geometry_type
+    arcpy.AddField_management(test_feature_class, "this_field_exists", "TEXT")
+    if field_in_feature_class("this_field_exists", test_feature_class):
+        print "  Pass"
+    else:
+        print "  Fail, %s field exists in %s, but tool returns False" % ("this_field_exists", test_feature_class)
+    print "Testing field_in_feature_class doesn't find a missing field..."
+    if field_in_feature_class("some_nonexistent_field", test_feature_class):
+        print "  Fail, %s field is not in %s, but tool returns True" % ("some_nonexistent_field", test_feature_class)
+        print "  Fields that exist:"
+        for field in arcpy.ListFields(test_feature_class):
+            print "    %s" % field.name
+    else:
+        print "  Pass"
+    delete_if_exists(test_feature_class)
     print "------"
 
     print "Testing return_tuple_of_args..."
